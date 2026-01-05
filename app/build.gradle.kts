b5e810831a92c65a086feffa43475c675d53d12f
Djay <the.djay2529@gmail.com>
2026-01-05 21:41:04 +0530

    Build Gradle fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,12 @@ dependencies {
 
     // Xposed compile-only API
     compileOnly(files("libs/xposed-api-82.jar"))
+
+    // Testing dependencies
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
 }
 
 configurations.all {
